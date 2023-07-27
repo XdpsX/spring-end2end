33892a852002a71bc3bd60ce7bceefe63bbda6f7
Night <dranhata@gmail.com>
2023-07-27 23:12:49 +0700

    verify token

--- src/main/java/com/spring/sbend2end/registration/RegistrationController.java
+++ src/main/java/com/spring/sbend2end/registration/RegistrationController.java
@@ -1,6 +1,8 @@
 package com.spring.sbend2end.registration;
 
 import com.spring.sbend2end.event.RegistrationCompleteEvent;
+import com.spring.sbend2end.registration.token.VerificationToken;
+import com.spring.sbend2end.registration.token.VerificationTokenService;
 import com.spring.sbend2end.user.IUserService;
 import com.spring.sbend2end.user.User;
 import com.spring.sbend2end.utility.UrlUtil;
@@ -9,10 +11,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @Controller
 @RequiredArgsConstructor
@@ -21,6 +22,7 @@ public class RegistrationController {
 
     private final IUserService userService;
     private final ApplicationEventPublisher publisher;
+    private final VerificationTokenService tokenService;
 
     @GetMapping("/registration-form")
     public String showRegistrationForm(Model model) {
@@ -35,5 +37,22 @@ public class RegistrationController {
         // publish the verification email event
         publisher.publishEvent(new RegistrationCompleteEvent(user, UrlUtil.getApplicationUrl(request)));
         return "redirect:/registration/registration-form?success";
+    }
+
+    @GetMapping("/verifyEmail")
+    public String verifyEmail(@RequestParam("token") String token) {
+        Optional<VerificationToken> theToken = tokenService.findByToken(token);
+        if (theToken.isPresent() && theToken.get().getUser().isEnabled()) {
+            return "redirect:/login?verified";
+        }
+        String verificationResult = tokenService.validateToken(token);
+        switch (verificationResult.toLowerCase()) {
+            case "expired":
+                return "redirect:/error?expired";
+            case "valid":
+                return "redirect:/login?valid";
+            default:
+                return "redirect:/error?invalid";
+        }
     }
 }
