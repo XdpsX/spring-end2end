b55c996ceb9a9711519778779c8bc1072460e2a5
Night <dranhata@gmail.com>
2023-07-27 17:59:59 +0700

    first commit

---
+++ src/main/java/com/spring/sbend2end/Sbend2endApplication.java
@@ -0,0 +1,13 @@
+package com.spring.sbend2end;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sbend2endApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sbend2endApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/sbend2end/Sbend2endApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.sbend2end;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sbend2endApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
